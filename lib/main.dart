8b82ba7469cc938c935f00e3c8f4e599af3fe43c
Sumit Kumar <sk4390789@gmail.com>
2021-11-20 19:52:01 +0530

    d generative RenderingFlutterBinding

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:fluttermath/learn/d_generative.dart';
 import 'package:fluttermath/screen/custome_painter_class_1.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
+// void main() => RenderingFlutterBinding(
+//       root: RenderPositionedBox(
+//         alignment: Alignment(.1, -.2),
+//         child: RenderCustomPaint(painter: Colored()),
+//       ),
+//     );
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
